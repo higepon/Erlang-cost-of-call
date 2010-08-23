68d04ff0e7c8ee88fdd82dadaa3fc028e2fc15d2
Higepon (MINOWA Taro) <higepon@gmail.com>
2010-08-23 15:34:24 +0900

    Tidy.

--- test.erl
+++ test.erl
@@ -52,6 +52,9 @@ do_times(N, Fun, Args) ->
     ok = apply(Fun, Args),
     do_times(N - 1, Fun, Args).
 
+do_parallel(Nproc, NtimesPerProc, Fun) ->
+    do_workers(Nproc, fun(_X) -> do_times(NtimesPerProc, fun() -> Fun(), ok end) end).
+
 test() ->
     Ntimes = 100000,
     {ok, Pid} = gen_server:start(test_server, [], []),
@@ -61,34 +64,40 @@ test() ->
     timeit("gen_server:call fun0 local", fun() -> gen_server:call(Pid, fun0) end, Ntimes),
     timeit("gen_server:call fun1 local", fun() -> gen_server:call(Pid, {fun1, "Hello"}) end, Ntimes),
     timeit("gen_server:call fun0 local 10 proc", fun() ->
-                                                         do_workers(10, fun(_X) -> do_times(Ntimes div 10, fun() -> gen_server:call(Pid, fun0), ok end) end)
+                                                         do_parallel(10, Ntimes div 10, fun () -> gen_server:call(Pid, fun0) end)
                                                  end, 1),
     timeit("gen_server:call fun0 local 100 proc", fun() ->
-                                                          do_workers(100, fun(_X) -> do_times(Ntimes div 100, fun() -> gen_server:call(Pid, fun0), ok end) end)
-                                                  end, 1),
+                                                          do_parallel(100, Ntimes div 100, fun () -> gen_server:call(Pid, fun0) end)
+                                                 end, 1),
     timeit("gen_server:call fun1_spawn local", fun() -> gen_server:call(Pid, fun1_spawn) end, Ntimes),
     timeit("gen_server:call fun1_spawn local 10 proc", fun() ->
-                                                               do_workers(10, fun(_X) -> do_times(Ntimes div 10, fun() -> gen_server:call(Pid, fun1_spawn), ok end) end)
+                                                               do_parallel(10, Ntimes div 10, fun () -> gen_server:call(Pid, fun1_spawn) end)
                                                        end, 1),
     timeit("gen_server:call fun1_spawn local 100 proc", fun() ->
-                                                                do_workers(100, fun(_X) -> do_times(Ntimes div 100, fun() -> gen_server:call(Pid, fun1_spawn), ok end) end)
-                                                        end, 1),
+                                                               do_parallel(100, Ntimes div 100, fun () -> gen_server:call(Pid, fun1_spawn) end)
+                                                       end, 1),
     timeit("gen_server:call fun0 remote", fun() -> gen_server:call(RemotePid, fun0) end, Ntimes),
     timeit("gen_server:call fun0 remote 10 proc", fun() ->
-                                                          do_workers(10, fun(_X) -> do_times(Ntimes div 10, fun() -> gen_server:call(RemotePid, fun0), ok end) end)
+                                                          do_parallel(10, Ntimes div 10, fun () -> gen_server:call(RemotePid, fun0) end)
                                                   end, 1),
     timeit("gen_server:call fun0 remote 100 proc", fun() ->
-                                                           do_workers(100, fun(_X) -> do_times(Ntimes div 100, fun() -> gen_server:call(RemotePid, fun0), ok end) end)
+                                                          do_parallel(100, Ntimes div 100, fun () -> gen_server:call(RemotePid, fun0) end)
+                                                   end, 1),
+    timeit("gen_server:call fun0 remote 200 proc", fun() ->
+                                                          do_parallel(200, Ntimes div 200, fun () -> gen_server:call(RemotePid, fun0) end)
                                                    end, 1),
     timeit("gen_server:call fun1 remote", fun() -> gen_server:call(RemotePid, {fun1, "Hello"}) end, Ntimes),
     timeit("gen_server:call fun1_spawn remote", fun() -> gen_server:call(RemotePid, fun1_spawn) end, Ntimes),
+    timeit("gen_server:call fun1_spawn remote 10 proc", fun() ->
+                                                                do_parallel(10, Ntimes div 10, fun () -> gen_server:call(RemotePid, fun1_spawn) end)
+                                                   end, 1),
     timeit("rpc:call fun0", fun() -> rpc:call(list_to_atom("test1@127.0.0.1"), test_server, fun0, []) end, Ntimes),
     timeit("rpc:call fun0 10 proc", fun() ->
-                                            do_workers(10, fun(_X) -> do_times(Ntimes div 10, fun() -> rpc:call(list_to_atom("test1@127.0.0.1"), test_server, fun0, []), ok end) end)
+                                            do_parallel(10, Ntimes div 10, fun () -> rpc:call(list_to_atom("test1@127.0.0.1"), test_server, fun0, []) end)
                                     end, 1),
-    timeit("processes rpc:call fun0 100 proc", fun() ->
-                                                       do_workers(100, fun(_X) -> do_times(Ntimes div 100, fun() -> rpc:call(list_to_atom("test1@127.0.0.1"), test_server, fun0, []), ok end) end)
-                                               end, 1),
+    timeit("rpc:call fun0 100 proc", fun() ->
+                                             do_parallel(100, Ntimes div 100, fun () -> rpc:call(list_to_atom("test1@127.0.0.1"), test_server, fun0, []) end)
+                                     end, 1),
     timeit("rpc:block_call fun0", fun() -> rpc:block_call(list_to_atom("test1@127.0.0.1"), test_server, fun0, []) end, Ntimes),
 
     halt(0).
